991b25438ffa913c5da495b968dae3bb0964e04f
stock <danielcstock12@gmail.com>
2022-09-18 23:51:30 -0300

    initial commit

---
+++ src/main/java/br/com/danielstock/alura/kakfa/NewOrderMain.java
@@ -0,0 +1,38 @@
+package br.com.danielstock.alura.kakfa;
+
+import org.apache.kafka.clients.producer.Callback;
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.common.serialization.StringSerializer;
+
+import java.util.Properties;
+import java.util.concurrent.ExecutionException;
+
+public class NewOrderMain {
+    public static void main(String[] args) throws ExecutionException, InterruptedException {
+        var producer = new KafkaProducer<String, String>(properties());
+        var topic = "ECOMMERCE_NEW_ORDER";
+        var value = "123";
+        var record = new ProducerRecord<String, String>(topic, value, value);
+        Callback callback = (data, ex) -> {
+            if (ex != null) {
+                ex.printStackTrace();
+                return;
+            }
+            System.out.println(data.topic() + ":::partition " + data.partition() + "/offset " + data.offset() + "/timestamp " + data.timestamp());
+        };
+        producer.send(record, callback).get();
+        var emailMessage = "Thanks! We are processing your order";
+        var emailRecord = new ProducerRecord<>(topic, emailMessage, emailMessage);
+        producer.send(emailRecord, callback).get();
+    }
+
+    private static Properties properties(){
+        var properties = new Properties();
+        properties.setProperty(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "127.0.0.1:9092");
+        properties.setProperty(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+        properties.setProperty(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+        return properties;
+    }
+}
